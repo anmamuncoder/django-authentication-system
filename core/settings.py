59f4c8adf5becce2fab9d7b0bc6b687ead928851
anmamun0 <anmamun0@gmail.com>
2025-11-02 14:59:51 +0600

    feat(settings): initial caching configuration with Redis broker
    Configured Django caching to use Redis as backend with django_redis. Local memory cache commented out for fallback. Enables faster caching and improved performance.

--- core/settings.py
+++ core/settings.py
@@ -177,3 +177,15 @@ SIMPLE_JWT = {
 # Run windows Terminal: celery -A core worker --loglevel=INFO -P solo
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+
+# Caching in Redis
+CACHES = {
+    "default": {
+        # "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+} 
